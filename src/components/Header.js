1d4b5aa1597255cfd3a9eff29f70ad214bf8873d
Ayush <ayush.gupta.jk@gmail.com>
2021-06-07 00:17:50 +0530

    👨‍💻 Added header in main page

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<header className="header">
+			<nav>
+				<h3>Ayush Gupta 👨‍💻</h3>
+				<div className="nav-links">
+					<a href="">About </a>
+					<a href="">Work</a>
+					<a href="">Contact</a>
+				</div>
+			</nav>
+		</header>
+	);
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../../styles/main.scss';
+import { Header } from '../components/Header';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Layout></Layout>
+			<Header />
 			<Component {...pageProps} />
 		</>
 	);
