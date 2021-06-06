d5bd85606108698b4379831fcde17776330e4822
Ayush <ayush.gupta.jk@gmail.com>
2021-06-07 01:08:41 +0530

    📦 Added some styling in avbout page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function About() {
 	return (
-		<div>
-			<h3>about</h3>
-		</div>
+		<main className="about" a>
+			<section className="mystory">
+				<h2>my story 👦</h2>
+				<article>
+					<p> I am a web developer based in India who loves making things for web</p>
+				</article>
+				<article>
+					<p>
+						I strive for simplicity, clarity and originality in my work through designs
+						where the role of every pixel is well thought and respected in development.
+					</p>
+				</article>
+			</section>
+		</main>
 	);
 }
