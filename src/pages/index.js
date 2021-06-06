1ae60b7e1dade8aa2f67219ea5250e730cffb04c
Ayush <ayush.gupta.jk@gmail.com>
2021-06-06 23:34:06 +0530

    🐵 Added social icons in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,51 @@
-import Head from 'next/head';
+// import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<>
-			<main className="homepage">
-				<div className="contentforhome">
-					<div className="content">
-						<section className="mainpagetext fade-in-bottom-text">
-							<div className="mainpage-heading">
-								<h3> web developer & designer</h3>
-								<h1>Ayush Gupta</h1>
+			<main className="index">
+				<section className="mainpagetext">
+					<h1>Ayush Gupta</h1>
+					<article className="mainpagetextpara">
+						<p>
+							I'm a self taught web developer specializing in building & occasionally
+							designing clean websites.
+						</p>
+					</article>
+					<div className="aboutme-homepage">
+						<a href="/about" className="about-tag">
+							<p> MORE ABOUT ME</p>
+							<div className="hovereffect">
+								<div className="right-arrow-btn "></div>
+								<i
+									className="fas fa-angle-right arrowbtnlogo "
+									id="arrowbtnlogo"></i>
 							</div>
-							<div className="mainpagetextpara">
-								<p>
-									I'm a self taught web developer specializing in building &
-									occasionally designing exceptional websites, applications, and
-									everything in between.
-								</p>
-							</div>
-							<section className="aboutme-homepage">
-								<a href="/about" className="about-tag">
-									<p> MORE ABOUT ME</p>
-									<div className="hovereffect">
-										<div className="right-arrow-btn "></div>
-										<i
-											className="fas fa-angle-right arrowbtnlogo "
-											id="arrowbtnlogo"></i>
-									</div>
-								</a>
-							</section>
-							<div className="email-button-container">
-								<a
-									id="email-button"
-									href="mailto:ayush.gupta.jk@gmail.com"
-									target="_blank">
-									Contact me
-								</a>
-							</div>
-						</section>
+						</a>
 					</div>
-				</div>
-				<div className="social-icons">
-					<a
-						href="https://www.instagram.com/_ayushguptaa/"
-						target="_blank"
-						rel="noopener">
-						<span>Ig</span>
-					</a>
-					<a href="https://github.com/Ayush6968" target="_blank" rel="noopener">
-						<span>Git</span>
-					</a>
-					<a
-						href="https://www.linkedin.com/in/ayush-gupta-54a855139/"
-						target="_blank"
-						rel="noopener"></a>
-				</div>
+				</section>
 			</main>
+			<footer className="social-icons">
+				<a href="https://www.instagram.com/_ayushguptaa/" target="_blank" rel="noopener">
+					<span>Ig</span>
+				</a>
+				<a href="https://github.com/Ayush6968" target="_blank" rel="noopener">
+					<span>Git</span>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/ayush-gupta-54a855139/"
+					target="_blank"
+					rel="noopener">
+					Li
+				</a>
+				<a
+					href="https://www.linkedin.com/in/ayush-gupta-54a855139/"
+					target="_blank"
+					rel="noopener">
+					Tw
+				</a>
+			</footer>
 		</>
 	);
 }
