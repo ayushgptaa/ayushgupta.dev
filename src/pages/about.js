d263f9c8f38b16bfc17c417b96d472d01be41b10
Ayush <ayush.gupta.jk@gmail.com>
2021-07-28 13:14:42 +0530

    🚀 Added work heading

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,46 +12,41 @@ export default function About() {
 					<h2>About me 👦</h2>
 					<article>
 						<p>
-							I am Ayush Gupta a web developer based in India 📍
-							who loves to create unique things for web 👨‍💻
+							I am Ayush Gupta a web developer based in India 📍 who loves to create unique things for web
+							👨‍💻
 						</p>
 					</article>
 
 					<article>
 						<p>
-							I didn't learned code by creating boring and average
-							websites, but I learned code by making amazing and
-							exceptional websites for some amazing people.
+							I didn't learned code by creating boring and average websites, but I learned code by making
+							amazing and exceptional websites for some amazing people.
 						</p>
 					</article>
 					<article>
 						<p>
-							I love to keep things simple and minimal but at the
-							same time I pay a lot of attention in details.
+							I love to keep things simple and minimal but at the same time I pay a lot of attention in
+							details.
 						</p>
 					</article>
 					<article>
 						<p>
-							Some of technologies I usually used are React.js,
-							NextJS, HTML5, CSS3, Sass, JavaScript and Node.js
-							and I design with Figma.
+							Some of technologies I usually used are React.js, NextJS, HTML5, CSS3, Sass, JavaScript and
+							Node.js and I design with Figma.
 						</p>
 					</article>
 					<article>
 						<p>
-							I strive to design interfaces and experiences that
-							people can enjoy on all digital mediums. I have a
-							strong basics of front-end and ui/ux design and
-							presently i am fixate towards imporving/learning
-							back-end skills.
+							I strive to design interfaces and experiences that people can enjoy on all digital mediums.
+							I have a strong basics of front-end and ui/ux design and presently i am fixate towards
+							imporving/learning back-end skills.
 						</p>
 					</article>
 
 					<article>
 						<p>
-							Other than creating websites, I love to sketching 🎨
-							anime 😻 photography 📷 travelling 🗼 food 🍕 and
-							music 🎶.
+							Other than creating websites, I love to sketching 🎨 anime 😻 photography 📷 travelling 🗼
+							food 🍕 and music 🎶.
 						</p>
 					</article>
 				</section>
@@ -62,9 +57,7 @@ export default function About() {
 
 					<h4>You can reach me here ✉</h4>
 					<h5>
-						<a href="mailto:ayush.gupta.jk@gmail.com">
-							ayush.gupta.jk@gmail.com
-						</a>
+						<a href="mailto:ayush.gupta.jk@gmail.com">ayush.gupta.jk@gmail.com</a>
 					</h5>
 				</section>
 				<Footer />
---
+++ src/pages/work.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import React from 'react';
+
+export default function work() {
+	return (
+		<>
+			<main className="work">
+				<section className="work-section">
+					<h2> Selected Works 🔨</h2>
+					<article>
+						<p>
+							A collection of my designs and front-end coding. These are a mix of freelance, practice and
+							personal projects.
+						</p>
+					</article>
+				</section>
+			</main>
+		</>
+	);
+}
