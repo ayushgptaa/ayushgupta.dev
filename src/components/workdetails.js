b92b502a031ebb29c45637198e45b1c8c3ee8aa0
Ayush <ayush.gupta.jk@gmail.com>
2021-08-01 20:38:30 +0530

    🐱‍🏍 changed work-about for whatsthesauce

--- src/components/workdetails.js
+++ src/components/workdetails.js
@@ -8,16 +8,19 @@ import WhatstheSauce from '../../public/wts.png';
 const workdetails = () => {
 	return (
 		<>
-			<div className="Work-info">
+			<div className="work-title">
 				<h3>Personal Project</h3>
 				<span></span>
+			</div>
+			<div className="Work-info">
 				<div className="work-image">
 					<Image src={WhatstheSauce} layout="fill" objectFit="cover" quality={100} />
 				</div>
 				<div className="work-details">
 					<h2 className="work-name">WHATSTHESAUCE</h2>
 					<p className="work-about">
-						An anime search engine that fetches anime details based on image provided
+						An anime search engine that fetches anime info based on image provided. It tells you which
+						anime, which episode, and the exact moment this scene appears.
 					</p>
 					<p className="work-stack">#Reactjs #Styledcomponents #graphql</p>
 					<SeeWork />
