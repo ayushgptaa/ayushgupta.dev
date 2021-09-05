2e0516a958933eb151b68ad5b5325b27b9752f50
Ayush <ayush.gupta.jk@gmail.com>
2021-09-05 21:18:59 +0530

    🗡 added google analytics in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import '../../styles/main.scss';
 import { Header } from '../components/Header';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 	useEffect(() => {
@@ -18,6 +19,22 @@ function MyApp({ Component, pageProps }) {
 	}, []);
 	return (
 		<>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script strategy="lazyOnload">
+				{`
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+					page_path: window.location.pathname,
+				});
+				`}
+			</Script>
 			<Header />
 			<Component {...pageProps}></Component>
 		</>
