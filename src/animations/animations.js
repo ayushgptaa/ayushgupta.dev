bbf98b2f5688f7dcda41ab2a7d30ef72e965c8c1
Ayush <ayush.gupta.jk@gmail.com>
2021-09-24 21:58:07 +0530

    🤏 minor changes in animations

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -1,34 +1,30 @@
 export const Homepganimation = () => {
-	const t1 = gsap.timeline({ delay: 0.2, ease: 'power1.out' });
+	const t1 = gsap.timeline({ delay: 0.3, ease: 'power4.out' });
 	t1.from('.header', {
 		y: -100,
-		duration: 1,
+		duration: 0.8,
 		opacity: 0,
 	})
 		.from('.social-icons', {
-			y: 100,
+			y: 50,
 			duration: 0.8,
 			opacity: 0,
 		})
 		.from('.mainpagetext', {
-			y: 100,
-			duration: 1,
-			ease: 'power1.out',
+			y: 50,
+			duration: 0.8,
+			ease: 'power4.out',
 			opacity: 0,
 		});
 };
 
 export const Aboutpganimation = () => {
-	const t2 = gsap.timeline({ delay: 0.2, ease: 'power1.out' });
+	const t2 = gsap.timeline({ delay: 0.2, ease: 'power4.out', opacity: 0, duration: 0.8 });
 	t2.from('.aboutmetext h1', {
 		y: 100,
-		duration: 0.8,
-		opacity: 0,
 		autoAlpha: 0,
 	}).from('.aboutmetext article', {
-		y: 100,
-		duration: 0.8,
-		opacity: 0,
+		y: 50,
 		stagger: 0.1,
 		autoAlpha: 0,
 	});
@@ -43,23 +39,19 @@ export const Workpganimations = () => {
 				autoAlpha: 0,
 				scrollTrigger: work,
 				ease: 'power1.out',
-				delay: 0.2,
+				delay: 0.1,
 				y: 100,
 				duration: 0.6,
 				opacity: 0,
 			});
 		});
 	};
-	const t3 = gsap.timeline({ delay: 0.5, ease: 'power1.out', onComplete: workinfo });
+	const t3 = gsap.timeline({ delay: 0.2, ease: 'power4.out', onComplete: workinfo, opacity: 0, duration: 0.8 });
 	t3.from('.work-section h1', {
 		y: 100,
-		duration: 0.8,
-		opacity: 0,
 		autoAlpha: 0,
 	}).from('.work-section article', {
-		y: 100,
-		duration: 0.8,
-		opacity: 0,
+		y: 50,
 		autoAlpha: 0,
 	});
 };
